bf7e6fa6496246c5c5ba314b9eee52720489b964
Paulo Lobo <pauloeduardolobo@gmail.com>
2018-09-17 17:48:50 -0300

    First version, plate conversion with console input/output.

---
+++ src/main/java/printers/Console.java
@@ -0,0 +1,20 @@
+package printers;
+
+import plate.Plate;
+
+/**
+ * Prints {@link Plate} objects to the console.
+ */
+public final class Console implements Media<Plate> {
+
+    private final Plate plate;
+
+    public Console(final Plate plate){
+        this.plate = plate;
+    }
+
+    @Override
+    public void print() {
+        System.out.println(plate.asString());
+    }
+}
